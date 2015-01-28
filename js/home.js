a4dad222e31735e5c52cb5bbede1e6fb4ada9690
Ben Weaver <Ben@benweaver.middlebury.edu>
2015-01-28 16:41:52 -0500

    changing api GET to d3 from ajax

--- js/home.js
+++ js/home.js
@@ -2,28 +2,25 @@ var $ = require("jquery");
 var Backbone = require("backbone");
 var hogan = require("hogan.js");
 Backbone.$ = $;
+var data;
 
 var HomeModel = Backbone.Model.extend({
   initialize: function() {
-    this.fetch();
+    fetch();
   },
   fetch: function() {
-    var me = this;
-    $.ajax({
-      url: 'http://startupgrid-api-staging.herokuapp.com/topics.json',
-      type: 'GET',
-      dataType: 'json'
-    })
-    .done(function(data) {
-      me.set("topics", data);
-    });
+    d3.json(""), function(error, json) {
+      if (error) return console.warn(error);
+      data = json;
+      window.console.log(data);
+    }
   }
 });
 
 var HomeView = Backbone.View.extend({
   initialize: function(params) {
-    this.router =params.router;
-    this.mode = new homeModel();
+    this.router = params.router;
+    this.model = new homeModel();
   },
   template: function() {
     return hogan.compile($("#template-home").html()).render(this.model.attributes);
